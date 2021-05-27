28b00e5797b1cc913eaf6e37f2d42f853aeced15
Michael Coyle <michael.david.coyle4@gmail.com>
2021-05-27 15:41:56 -0500

    completed sayHello(myName)

--- js/functions.js
+++ js/functions.js
@@ -32,6 +32,10 @@ console.log(helloMessage);
  * console.
  */
 
+var myName = "Michael";
+
+console.log(sayHello(myName));
+
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
 var random = Math.floor((Math.random() * 3) + 1);
