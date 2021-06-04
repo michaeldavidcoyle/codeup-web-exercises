0f109c0727f06bd5463f3f239e3c009acce7010e
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-04 15:37:02 -0500

    Commented do/while loop exercise description

--- js/while.js
+++ js/while.js
@@ -25,3 +25,13 @@ while (int < 65536) {
     int *= 2;
     console.log(int);
 }
+
+/*
+ An ice cream seller can't go home until she sells all of her cones. First write
+ enough code that generates a random number between 50 and 100 representing the
+ amount of cones to sell before you start your loop. Inside of the loop your
+ code should generate another random number between 1 and 5, simulating the
+ amount of cones being bought by her clients. Use a do-while loop to log to the
+ console the amount of cones sold to each person. This is a way get the random
+ numbers for this exercise.
+*/
