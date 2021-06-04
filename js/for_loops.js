c4090fe4b69d4074da63b422f178fc253247f121
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-04 10:24:41 -0500

    Finished showMultiplicationTable function. Tested in console.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function showMultiplicationTable(integer) {
+    var table = '',
+        product;
+
+    for (var i = 1; i <= 10; i++) {
+        product = integer * i;
+        table += integer + ' x ' + i + ' = ' + product + "\n";
+    }
+
+    console.log(table);
+}
