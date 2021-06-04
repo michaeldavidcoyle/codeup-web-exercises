47ffeacf092f4a38e8ae240dcd9db0be7bdd4f78
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-04 15:28:47 -0500

    Added exercise descriptions in comments

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Prompt the user for an odd number between 1 and 50. Use a loop and a break
+// statement to continue prompting the user if they enter invalid input.
 var inputIsValid = true;
 var userNumber;
 
@@ -11,6 +13,8 @@ while (inputIsValid) {
     }
 }
 
+// Use a loop and the continue statement to output all the odd numbers between
+// 1 and 50, except for the number the user entered.
 for (var i = 1; i < 50;  i++) {
     if (i % 2 === 0) {
         continue;
