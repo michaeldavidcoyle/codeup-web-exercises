883b84abd67349ad860e4e85eb6d4fbaf9a3f0d4
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-04 15:00:40 -0500

    Got the loop working and break when input is valid

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,2 +1,14 @@
 "use strict";
 
+var inputIsValid = true;
+var input;
+
+while (inputIsValid) {
+    input = +prompt('Enter an odd number between 1 and 50');
+
+    if (!isNaN(input) && input % 2 === 1) {
+        break;
+    }
+}
+
+console.log('Yay!');
