6506b6899852b15dc0f06a567b2812a181e86174
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-04 10:36:43 -0500

    Solved for loop #3, generate 10 random numbers and output if odd or even

--- js/for_loops.js
+++ js/for_loops.js
@@ -16,3 +16,13 @@ function showMultiplicationTable(integer) {
 for (var i = 1; i <= 10; i++) {
     showMultiplicationTable(i);
 }
+
+for (var i = 0; i < 10; i++) {
+    var random = Math.floor(Math.random() * (200 - 20 + 1) + 20);
+
+    if (random % 2 === 0) {
+        console.log(random + ' is even');
+    } else {
+        console.log(random + ' is odd');
+    }
+}
