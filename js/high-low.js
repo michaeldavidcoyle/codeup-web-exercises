8e521deee8a1649dd6237868f638c0e61baeaee1
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-06 22:50:13 -0500

    Fixed script src attribute and added confirm asking user if they want to play

--- js/high-low.js
+++ js/high-low.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var wantsToPlay = confirm('Would you like to play the high/low game?');
+
+if (wantsToPlay) {
+    alert('Cool, let me finish coding the game.');
+} else {
+    alert('No hard feelings. Goodbye.')
+}
