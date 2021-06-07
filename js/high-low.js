cce5552333476bccdd6701f37d720ed7eb8b0a72
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-06 23:23:02 -0500

    Added variable and prompt to allow user to input max value

--- js/high-low.js
+++ js/high-low.js
@@ -3,8 +3,9 @@
 var wantsToPlay = confirm('Would you like to play the high/low game?');
 
 if (wantsToPlay) {
-    var myNumber = Math.ceil(Math.random() * 100);
-    var guess = +prompt("I'm thinking of a number from 1 to 100. Can you guess my number?");
+    var max = +prompt('Enter a maximum number: ');
+    var myNumber = Math.ceil(Math.random() * max);
+    var guess = +prompt("I'm thinking of a number from 1 to " + max + ". Can you guess my number?");
 
     while (guess != myNumber) {
         if (guess < myNumber) {
@@ -14,7 +15,7 @@ if (wantsToPlay) {
         }
     }
 
-    alert('Yes! You guessed my number, ' + myNumber + '\n. Refresh to play again.');
+    alert('Yes! You guessed my number, ' + myNumber + '.\nRefresh to play again.');
 } else {
     alert('No hard feelings. Goodbye.')
 }
