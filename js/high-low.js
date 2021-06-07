df00c013bdfedbb530f8988cf905786cdd4d45c2
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-06 23:02:42 -0500

    Added prompt to ask for user guess

--- js/high-low.js
+++ js/high-low.js
@@ -4,7 +4,9 @@ var wantsToPlay = confirm('Would you like to play the high/low game?');
 
 if (wantsToPlay) {
     var myNumber = Math.ceil(Math.random() * 100);
-    console.log('My number is ' + myNumber);
+    var guess = +prompt("I'm thinking of a number from 1 to 100. Can you guess my number?");
+
+    console.log(guess);
 } else {
     alert('No hard feelings. Goodbye.')
 }
