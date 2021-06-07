1e1ea85fb77f8289b18c394d7d8cf785766b20c1
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-06 23:35:55 -0500

    Added do/while loop to ensure user inputs number

--- js/high-low.js
+++ js/high-low.js
@@ -3,7 +3,10 @@
 var wantsToPlay = confirm('Would you like to play the high/low game?');
 
 if (wantsToPlay) {
-    var max = +prompt('Enter a maximum number (whole numbers only please).\nThe higher the number, the longer the game.');
+    do {
+        var max = parseInt(prompt('Enter a maximum number (whole numbers only please).\nThe higher the number, the longer the game.'));
+    } while (isNaN(max));
+
     var myNumber = Math.ceil(Math.random() * max);
     var guess = +prompt("Alright, I'm thinking of a number from 1 to " + max + ". Can you guess my number?");
 
