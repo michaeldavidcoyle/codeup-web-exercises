1bb39d8df7f56f0632099a89203d77ec09b2aef3
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-07 09:59:27 -0500

    Added console log to output number to skip

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -15,6 +15,7 @@ while (inputIsValid) {
 
 // Use a loop and the continue statement to output all the odd numbers between
 // 1 and 50, except for the number the user entered.
+console.log('Number to skip is: ' + userNumber);
 for (var i = 1; i < 50;  i++) {
     if (i % 2 === 0) {
         continue;
