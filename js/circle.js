e4519bce1cf1e4da30e438e05b62ccedf371a64f
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-09 11:26:24 -0500

    Completed logInfo method on circle object

--- js/circle.js
+++ js/circle.js
@@ -10,12 +10,15 @@
         },
 
         logInfo: function (doRounding) {
-            // TODO: complete this method.
-
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
+            var area = this.getArea();
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding) {
+                area = Math.round(area);
+            }
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
     };
 
