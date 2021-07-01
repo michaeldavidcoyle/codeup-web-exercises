f347bb909741251e50573145ea975c9b52ec55ad
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-01 09:21:03 -0500

    Completed validation

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -3,7 +3,11 @@
 // Create a function named countSpaces that accepts a string and returns
 //  the total number of spaces in the string.
 
+// Add validation to function, if the argument pass is not a string
+//  it should return false.
+
 function countSpaces(string) {
+    if (typeof string !== 'string') return false;
     var count = 0;
     for (let index = 0; index < string.length; index++) {
         if (string[index] == ' ') count++;
@@ -12,5 +16,3 @@ function countSpaces(string) {
     return count;
 }
 
-// TODO: Add validation to function above, if the argument pass is not a string
-//  it should return false.
