ed05d730ad551fb3d43340e8d36abdada97ff2b0
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-01 09:11:47 -0500

    Set up files for morning warm up exercise

---
+++ js/morningWarmUps.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// TODO: Create a function named countSpaces that accepts a string and returns
+//  the total number of spaces in the string.
+
+
+// TODO: Add validation to function above, if the argument pass is not a string
+//  it should return false.
