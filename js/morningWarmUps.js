bad6155f81b3706703a01bb3b79431e718807f3f
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-08 09:17:04 -0500

    Completed first TODO

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -114,3 +114,46 @@ var personThree = {
 }
 
 console.log(personThree.intro());
+
+// July 8th 2021
+// Part 1
+// Create a function that takes an array of strings and returns an array of objects
+//  where each object has 2 properties: originalString and countOfLetters
+// Example: input: ["hello", "dave"]
+//          returns: [
+//                    { "originalString":"hello", lengthOfOriginalString:5 },
+//                    { "originalString":"dave", lengthOfOriginalString:4 }
+//                   ]
+// Part 2
+// TODO: Create a function that takes the array of objects from PART 1
+//  and returns a string of all of the originalStrings concatenated with spaces in between
+// Example: input: [
+//                    { "originalString":"hello", lengthOfOriginalString:5 },
+//                    { "originalString":"dave", lengthOfOriginalString:4 }
+//                  ]
+//          returns: "hello dave"
+// TODO: Create a function named getTallUsers that accepts an array of objects. The objects in the array will be in the same format
+//  as PersonOne, PersonTwo, and PersonThree. The functions job is to return an array of users that have heightInInches equal to or greater than 65.
+var people = [personOne,personTwo,personThree];
+// Example: getTallUsers(people)
+// ---- returns ----> [{firstName: "silvia", lastName: "floopertan", ageInYears: 34, heightInInches: 65},
+//
+
+function stringsToObjects(stringsArray) {
+    var objectsArray = [],
+        string;
+
+    for (var index = 0; index < stringsArray.length; index++) {
+        string = stringsArray[index];
+        objectsArray.push(
+            {
+                originalString: string,
+                countOfLetters: string.length
+            }
+        );
+    }
+
+    return objectsArray;
+}
+
+console.log(stringsToObjects(["hello", "dave"]));
