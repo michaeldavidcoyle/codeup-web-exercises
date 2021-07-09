7ef1081b15f990dda1edb086910649e6cdd94a1d
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-09 14:26:39 -0500

    Element Selectors, Part 3

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -9,4 +9,11 @@ $(document).ready(function() {
     // $('.codeup').css('border', '1px solid red');
 
     $('li').css('font-size', '20px');
+
+    // not sure if this is what the instructions are asking for
+    // $('h1, p, li').css('background-color', 'yellowgreen');
+    // or this
+    $('h1').css('background-color', 'ivory');
+    $('p').css('background-color', 'yellow');
+    $('li').css('background-color', 'yellowgreen');
 });
