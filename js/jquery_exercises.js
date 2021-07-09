34f1bd6c3191b73c4ebd912bb67ac3b9de866b63
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-09 14:17:29 -0500

    Parts 2 and 3 of Class Selectors section

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,6 +3,8 @@
 $(document).ready(function() {
     // alert('The DOM has finished loading.');
 
-    alert( $('#pop').html() );
+    // alert( $('#pop').html() );
     // alert( $('#list-no-style').html() );
+
+    $('.codeup').css('border', '1px solid red');
 });
