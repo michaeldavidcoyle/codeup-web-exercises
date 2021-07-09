ce41ffaf56930b7b01a05431a0373c47f3e1800f
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-09 14:14:02 -0500

    Part 5

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,6 +3,6 @@
 $(document).ready(function() {
     // alert('The DOM has finished loading.');
 
-    // alert( $('#pop').html() );
-    alert( $('#list-no-style').html() );
+    alert( $('#pop').html() );
+    // alert( $('#list-no-style').html() );
 });
