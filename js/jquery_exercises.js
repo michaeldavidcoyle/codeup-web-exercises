f87a71250529433bc1628c7e6e941a4a21df5e53
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-09 14:29:24 -0500

    Multiple selectors done

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -10,12 +10,11 @@ $(document).ready(function() {
 
     $('li').css('font-size', '20px');
 
-    // not sure if this is what the instructions are asking for
-    // $('h1, p, li').css('background-color', 'yellowgreen');
-    // or this
     $('h1').css('background-color', 'ivory');
     $('p').css('background-color', 'yellow');
     $('li').css('background-color', 'yellowgreen');
 
     alert($('h1').html());
+
+    $('h1, p, li').css('background-color', 'yellowgreen');
 });
