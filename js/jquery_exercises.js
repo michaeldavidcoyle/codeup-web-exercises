562f635293512b0430178d91cb712c8726f14fca
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-09 14:12:27 -0500

    Completed parts 3 and 4 of the ID selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,8 @@
 "use strict";
 
 $(document).ready(function() {
-    alert('The DOM has finished loading.');
+    // alert('The DOM has finished loading.');
+
+    // alert( $('#pop').html() );
+    alert( $('#list-no-style').html() );
 });
