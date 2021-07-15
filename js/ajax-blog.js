cb380ded118be4ba6cf9fb68d4c815514d859acf
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-14 23:23:20 -0500

    Added h1, made ajax request and loaded contents into posts div

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -1,5 +1,21 @@
 "use strict";
 
 $(document).ready(function() {
-    alert("Let's get this show on the road.");
+    // alert("Let's get this show on the road.");
+
+    var posts = $('#posts');
+
+    $.get("data/blog.json", {
+        limit:  10,
+        offset: 20
+    }).done(function(data) {
+        data.forEach(function(datum) {
+            $('#posts').append(
+                '<h2>' + datum['title'] + '</h2>',
+                '<p>' + datum['date'] + '</p>',
+                '<p>' + datum['content'] + '</p>',
+                '<p>' + datum['categories'].join(', ') + '</p>'
+            );
+        });
+    });
 });
