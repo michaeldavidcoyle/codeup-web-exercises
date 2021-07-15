301c9c2d30d21356bffdc3d1b5c649512a6517b7
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-14 23:44:59 -0500

    Moved JS to separate file, ajax-store.js

---
+++ js/ajax-store.js
@@ -0,0 +1,39 @@
+(function() {
+    "use strict";
+    // Create an AJAX GET request for the file under data/inventory.json
+
+    // Take the data from inventory.json and append it to the products table
+    //       HINT: Your data should come back as a JSON object; use console.log() to inspect
+    //             its contents and fields
+    //       HINT: You will want to target #insertProducts for your new HTML elements
+    var tableBody = $('#insertProducts');
+
+    function getAndLoadJson(data) {
+        // console.log('Inventory: ');
+        // console.log(data);
+        data.forEach(function(datum) {
+            var columns = [
+                datum.title, datum.quantity, datum.price, datum.categories.join(', ')
+            ];
+            tableBody.append('<tr><td>' + columns.join('</td><td>') + '</td></tr>');
+        });
+    }
+
+    $.get("data/inventory.json", {
+        limit:  10,
+        offset: 20
+    }).done(getAndLoadJson);
+
+    $('#refresh').click(function() {
+        tableBody.html('');
+
+        $.get("data/inventory.json", {
+            limit:  10,
+            offset: 20
+        }).done(getAndLoadJson);
+    });
+
+    tableBody.children().each(function(index) {
+        console.log($(this));
+    });
+})();
