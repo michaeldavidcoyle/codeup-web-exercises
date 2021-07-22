d9708875b44cff85915557a6169ef0fd15108d39
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-22 11:31:33 -0500

    Completed next 3 TODOs

--- js/es6.js
+++ js/es6.js
@@ -2,31 +2,31 @@
  * Complete the TODO items below
  */
 const users = [
-  {
-    name: 'zach',
-    email: 'zach@codeup.com',
-    languages: ['javascript', 'bash']
-  },
-  {
-    name: 'ryan',
-    email: 'ryan@codeup.com',
-    languages: ['clojure', 'javascript']
-  },
-  {
-    name: 'luis',
-    email: 'luis@codeup.com',
-    languages: ['java', 'scala', 'php']
-  },
-  {
-    name: 'fernando',
-    email: 'fernando@codeup.com',
-    languages: ['java', 'php', 'sql']
-  },
-  {
-    name: 'justin',
-    email: 'justin@codeup.com',
-    languages: ['html', 'css', 'javascript', 'php']
-  }
+    {
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash']
+    },
+    {
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript']
+    },
+    {
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php']
+    },
+    {
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql']
+    },
+    {
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php']
+    }
 ];
 
 // TODO: fill in your name and email and add some programming languages you know
@@ -40,9 +40,9 @@ const languages = ['html', 'css', 'javascript', 'php'];
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name,
-  email,
-  languages
+    name,
+    email,
+    languages
 });
 
 console.log(users[users.length - 1]);
@@ -66,18 +66,22 @@ console.log(emails);
 console.log(names);
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
-  // TODO: rewrite the code below to use object destructuring assignment
-  //       note that you can also use destructuring assignment in the function
-  //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+    // TODO: rewrite the code below to use object destructuring assignment
+    //       note that you can also use destructuring assignment in the function
+    //       parameter definition
+    // const name = user.name;
+    // const email = user.email;
+    // const languages = user.languages;
+    const {name, email, languages} = user;
 
-  // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+    // TODO: rewrite the assignment below to use template strings
+    // developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+    developers.push(`${name}'s email is ${email}. ${name} knows ${languages.join(', ')}`);
 });
+
+console.log(developers);
 
 // TODO: Use `let` for the following variable
 var list = '<ul>';
@@ -85,7 +89,7 @@ var list = '<ul>';
 // TODO: rewrite the following loop to use a for..of loop
 developers.forEach(function (developer) {
 
-  // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
+    // TODO: rewrite the assignment below to use template strings
+    list += '<li>' + developer + '</li>';
 });
 list += '</ul>';
