786c5d48cc450c8eb6ee28da91330b0bf8c39fbe
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-23 13:56:27 -0500

    Completed first 3 parts of exercises, getting date of user last commit

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,13 @@
 "use strict";
 
+function getLastCommitDate(username) {
+    fetch(`https://api.github.com/users/${username}/events`, {
+        headers: {'Authorization': `token ${GITHUB_PA_TOKEN}`
+        }
+    }).then(response => {
+        return response.json();
+    }).then(data => console.log('Date of last commit: ', data[0].created_at))
+        .catch(error => console.error('Oops, something went wrong: ', error));
+}
+
+getLastCommitDate('michaeldavidcoyle');
