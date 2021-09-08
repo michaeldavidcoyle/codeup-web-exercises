617b863fd8c71fe29e2d899c1e4eba407761b372
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-07 23:33:31 -0500

    Generated object CHARS with arrays for lowercase, uppercase, numbers and symbols.

--- js/password-generator.js
+++ js/password-generator.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const CHARS = {
+    lower: [],
+    upper: [],
+    numbers: [],
+    symbols: []
+}
+
+for (let charCode = 33; charCode < 127; charCode++) {
+    let char = String.fromCharCode(charCode);
+
+    if (/[a-z]/.test(char)) {
+        CHARS.lower.push(char);
+    } else if (/[A-Z]/.test(char)) {
+        CHARS.upper.push(char);
+    } else if (/[0-9]/.test(char)) {
+        CHARS.numbers.push(char);
+    } else {
+        CHARS.symbols.push(char);
+    }
+}
+
+
