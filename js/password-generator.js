399dfafaa2a5dd6029ea56dc274496597b7e0c1c
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-10 22:56:20 -0500

    Placed everything inside .ready

--- js/password-generator.js
+++ js/password-generator.js
@@ -1,63 +1,72 @@
 "use strict";
 
-function lengthHandler(event) {
-    // console.log(this.value);
-    lengthOutput.html($(this).val());
-    passwordLength = $(this).val();
-}
-
-const CHARS = {
-    lower: [],
-    upper: [],
-    numbers: [],
-    symbols: []
-}
-
-for (let charCode = 33; charCode < 127; charCode++) {
-    let char = String.fromCharCode(charCode);
-
-    if (/[a-z]/.test(char)) {
-        CHARS.lower.push(char);
-    } else if (/[A-Z]/.test(char)) {
-        CHARS.upper.push(char);
-    } else if (/[0-9]/.test(char)) {
-        CHARS.numbers.push(char);
-    } else {
-        CHARS.symbols.push(char);
+$(document).ready(function () {
+    function lengthHandler(event) {
+        // console.log(this.value);
+        lengthOutput.html($(this).val());
+        passwordLength = $(this).val();
     }
-}
 
-const lengthInput = $('#password-length');
+    function lowerCountHandler() {
+        lowerOutput.html($(this).val());
+        lowerLength = $(this).val();
+    }
 
-const includeLower = $('#include-lower');
-const includeUpper = $('#include-upper');
-const includeNumbers = $('#include-numbers');
-const includeSymbols = $('#include-symbols');
+    const CHARS = {
+        lower: [],
+        upper: [],
+        numbers: [],
+        symbols: []
+    }
 
-const lowerCount = $('#lower-count');
-const upperCount = $('#upper-count');
-const numbersCount = $('#numbers-count');
-const symbolsCount = $('#symbols-count');
+    for (let charCode = 33; charCode < 127; charCode++) {
+        let char = String.fromCharCode(charCode);
 
-const lowerFormGroup = $('#lower-form-group');
-const upperFormGroup = $('#upper-form-group');
-const numbersFormGroup = $('#numbers-form-group');
-const symbolsFormGroup = $('#symbols-form-group');
+        if (/[a-z]/.test(char)) {
+            CHARS.lower.push(char);
+        } else if (/[A-Z]/.test(char)) {
+            CHARS.upper.push(char);
+        } else if (/[0-9]/.test(char)) {
+            CHARS.numbers.push(char);
+        } else {
+            CHARS.symbols.push(char);
+        }
+    }
 
-const lengthOutput = $('#length-output');
-const lowerOutput = $('#lower-count-output');
-const upperOutput = $('#upper-count-output');
-const numbersOutput = $('#numbers-count-output');
-const symbolsOutput = $('#symbols-count-output');
+    const lengthInput = $('#password-length');
 
-let passwordLength = 8;
-let upperLength = 1;
-let numbersLength = 1;
-let symbolsLength = 2;
+    const includeLower = $('#include-lower');
+    const includeUpper = $('#include-upper');
+    const includeNumbers = $('#include-numbers');
+    const includeSymbols = $('#include-symbols');
 
-includeLower.change(() => lowerFormGroup.toggleClass("d-none"));
-includeUpper.change(() => upperFormGroup.toggleClass("d-none"));
-includeNumbers.change(() => numbersFormGroup.toggleClass("d-none"));
-includeSymbols.change(() => symbolsFormGroup.toggleClass("d-none"));
+    const lowerCount = $('#lower-count');
+    const upperCount = $('#upper-count');
+    const numbersCount = $('#numbers-count');
+    const symbolsCount = $('#symbols-count');
 
-lengthInput.change(lengthHandler);
+    const lowerFormGroup = $('#lower-form-group');
+    const upperFormGroup = $('#upper-form-group');
+    const numbersFormGroup = $('#numbers-form-group');
+    const symbolsFormGroup = $('#symbols-form-group');
+
+    const lengthOutput = $('#length-output');
+    const lowerOutput = $('#lower-count-output');
+    const upperOutput = $('#upper-count-output');
+    const numbersOutput = $('#numbers-count-output');
+    const symbolsOutput = $('#symbols-count-output');
+
+    let passwordLength = 8;
+    let lowerLength = 5;
+    let upperLength = 1;
+    let numbersLength = 1;
+    let symbolsLength = 1;
+
+    includeLower.change(() => lowerFormGroup.toggleClass("d-none"));
+    includeUpper.change(() => upperFormGroup.toggleClass("d-none"));
+    includeNumbers.change(() => numbersFormGroup.toggleClass("d-none"));
+    includeSymbols.change(() => symbolsFormGroup.toggleClass("d-none"));
+
+    lengthInput.change(lengthHandler);
+    lowerCount.change(lowerCountHandler);
+});
