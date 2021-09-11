061826c84f6b5923e4ed0314f00260573e6a4c89
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-10 21:51:57 -0500

    Switched all the DOM stuff to jQuery.

--- js/password-generator.js
+++ js/password-generator.js
@@ -2,7 +2,7 @@
 
 function lengthHandler(event) {
     // console.log(this.value);
-    lengthOutput.innerText = this.value;
+    lengthOutput.html(this.value);
     passwordLength = this.value;
 }
 
@@ -27,23 +27,23 @@ for (let charCode = 33; charCode < 127; charCode++) {
     }
 }
 
-const lengthInput = document.getElementById('password-length');
+const lengthInput = $('#password-length');
 
-const includeLower = document.getElementById('include-lower');
-const includeUpper = document.getElementById('include-upper');
-const includeNumbers = document.getElementById('include-numbers');
-const includeSymbols = document.getElementById('include-symbols');
+const includeLower = $('#include-lower');
+const includeUpper = $('#include-upper');
+const includeNumbers = $('#include-numbers');
+const includeSymbols = $('#include-symbols');
 
-const lowerCount = document.getElementById('lower-count');
-const upperCount = document.getElementById('upper-count');
-const numbersCount = document.getElementById('numbers-count');
-const symbolsCount= document.getElementById('symbols-count');
+const lowerCount = $('#lower-count');
+const upperCount = $('#upper-count');
+const numbersCount = $('#numbers-count');
+const symbolsCount = $('#symbols-count');
 
-const lengthOutput = document.getElementById('length-output');
-const lowerOutput = document.getElementById('lower-count-output');
-const upperOutput = document.getElementById('upper-count-output');
-const numbersOutput = document.getElementById('numbers-count-output');
-const symbolsOutput = document.getElementById('symbols-count-output');
+const lengthOutput = $('#length-output');
+const lowerOutput = $('#lower-count-output');
+const upperOutput = $('#upper-count-output');
+const numbersOutput = $('#numbers-count-output');
+const symbolsOutput = $('#symbols-count-output');
 
 let passwordLength = 8;
 let upperLength = 1;
