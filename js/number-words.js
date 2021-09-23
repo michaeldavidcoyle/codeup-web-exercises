4ba30cbef48b4ad77a91146b2b4f01ce5c047f77
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-22 22:09:01 -0500

    Set up files for a small app to convert numbers from their numeric representations into words.

---
+++ js/number-words.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const teens = [
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+];
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
