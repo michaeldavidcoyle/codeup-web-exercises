9f75557367dfb517735e2d723cbdc201e84e4b19
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-22 23:32:16 -0500

    Used loops to build array of all numbers 0-99 and added function to convert three-digit groups.

--- js/number-words.js
+++ js/number-words.js
@@ -1,7 +1,31 @@
 "use strict";
 
+function threeDigitGroup(number) {
+    let words = '';
+    let lastTwo = number % 100;
+    let first = (number - lastTwo) / 100;
+
+    if (first > 0) {
+        words += `${units[first]} hundred`;
+    }
+
+    if (lastTwo > 0) {
+        words += ` ${twoDigits[lastTwo]}`;
+    }
+
+    return words;
+}
+
 const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-const teens = [
-    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
-];
+const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const twoDigits = units.slice(0).concat(teens);
+
+for (let t = 2; t < 10; t++) {
+    for (let u = 0; u < 10; u++) {
+        let word = (u > 0) ? `${tens[t]}-${units[u]}` : tens[t];
+        twoDigits.push(word);
+    }
+}
+
+for (let i = 0; i < 300; i++) console.log(threeDigitGroup(i));
