f64af7ee292daae0e5eeb414bcddb32c16dafe98
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-24 22:15:00 -0500

    Changed groupToWords to use an array instead of a string. Created function to handle larger numbers.

--- js/number-words.js
+++ js/number-words.js
@@ -1,19 +1,25 @@
 "use strict";
 
+function numberToWords(number) {
+    let groups = groupByThree(number).reverse().map((group, i) => groupToWords(group) + thousands[i]);
+
+    return groups.reverse().join(' ');
+}
+
 function groupToWords(number) {
-    let words = '';
+    let words = [];
     let lastTwo = number % 100;
     let first = (number - lastTwo) / 100;
 
     if (first > 0) {
-        words += `${units[first]} hundred`;
+        words.push(units[first], 'hundred');
     }
 
     if (lastTwo > 0) {
-        words += ` ${twoDigits[lastTwo]}`;
+        words.push(twoDigits[lastTwo]);
     }
 
-    return words;
+    return words.join(' ');
 }
 
 function groupByThree(number) {
@@ -30,6 +36,8 @@ function groupByThree(number) {
 const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const thousands = ['', ' thousand', ' million', ' billion', ' trillion', ' quadrillion'];
+
 const twoDigits = units.slice(0).concat(teens);
 
 for (let t = 2; t < 10; t++) {
