7bb953bce8d2aa88a897dd0ac31333e0f9261cd1
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-25 19:47:37 -0500

    Placed all JS inside an IIFE.

--- js/number-words.js
+++ js/number-words.js
@@ -1,79 +1,81 @@
 "use strict";
 
-function numberToWords(number, isNegative) {
-    let groups = groupByThree(number).reverse();
-    let words = [];
+(function() {
+    function numberToWords(number, isNegative) {
+        let groups = groupByThree(number).reverse();
+        let words = [];
 
-    groups.forEach((group, i) => {
-        if (group > 0) {
-            words.push(thousands[i], groupToWords(group));
+        groups.forEach((group, i) => {
+            if (group > 0) {
+                words.push(thousands[i], groupToWords(group));
+            }
+        });
+
+        if (isNegative) words.push('negative');
+
+        return words.reverse().join(' ');
+    }
+
+    function groupToWords(number) {
+        let words = [];
+        let lastTwo = number % 100;
+        let first = (number - lastTwo) / 100;
+
+        if (first > 0) {
+            words.push(units[first], 'hundred');
         }
-    });
 
-    if (isNegative) words.push('negative');
+        if (lastTwo > 0) {
+            words.push(twoDigits[lastTwo]);
+        }
 
-    return words.reverse().join(' ');
-}
-
-function groupToWords(number) {
-    let words = [];
-    let lastTwo = number % 100;
-    let first = (number - lastTwo) / 100;
-
-    if (first > 0) {
-        words.push(units[first], 'hundred');
+        return words.join(' ');
     }
 
-    if (lastTwo > 0) {
-        words.push(twoDigits[lastTwo]);
+    function groupByThree(number) {
+        let groups = [];
+        let digits = number.toString().split('');
+
+        while (digits.length > 0) {
+            groups.unshift(digits.splice(-3, 3).join(''));
+        }
+
+        return groups.map(group => +group);
     }
 
-    return words.join(' ');
-}
+    function inputHandler(event) {
+        event.preventDefault();
+        error.classList.add('hidden');
 
-function groupByThree(number) {
-    let groups = [];
-    let digits = number.toString().split('');
+        let isNegative = numberInput.value < 0;
+        let number = Math.abs(numberInput.value);
 
-    while (digits.length > 0) {
-        groups.unshift(digits.splice(-3, 3).join(''));
+        if (Number.isSafeInteger(number)) {
+            output.innerText = numberToWords(number, isNegative);
+        } else {
+            error.classList.remove('hidden');
+        }
     }
 
-    return groups.map(group => +group);
-}
+    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const thousands = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion'];
 
-function inputHandler(event) {
-    event.preventDefault();
-    error.classList.add('hidden');
+    const twoDigits = units.slice(0).concat(teens);
 
-    let isNegative = numberInput.value < 0;
-    let number = Math.abs(numberInput.value);
-
-    if (Number.isSafeInteger(number)) {
-        output.innerText = numberToWords(number, isNegative);
-    } else {
-        error.classList.remove('hidden');
+    for (let t = 2; t < 10; t++) {
+        for (let u = 0; u < 10; u++) {
+            let word = (u > 0) ? `${tens[t]}-${units[u]}` : tens[t];
+            twoDigits.push(word);
+        }
     }
-}
 
-const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-const thousands = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion'];
+    const numberInput = document.getElementById('number');
+    const output = document.getElementById('output');
+    const submit = document.getElementById('submit');
+    const error = document.getElementById('error');
 
-const twoDigits = units.slice(0).concat(teens);
-
-for (let t = 2; t < 10; t++) {
-    for (let u = 0; u < 10; u++) {
-        let word = (u > 0) ? `${tens[t]}-${units[u]}` : tens[t];
-        twoDigits.push(word);
-    }
-}
-
-const numberInput = document.getElementById('number');
-const output = document.getElementById('output');
-const submit = document.getElementById('submit');
-const error = document.getElementById('error');
-
-numberInput.addEventListener('change', inputHandler);
-submit.addEventListener('click', inputHandler);
+    numberInput.addEventListener('change', inputHandler);
+    submit.addEventListener('click', inputHandler);
+})();
